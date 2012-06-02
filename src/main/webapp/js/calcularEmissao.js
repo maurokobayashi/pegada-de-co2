4038349faea5d20559cb3f9a0767f7c5072dcce8
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2012-06-02 14:33:48 -0300

    refactoring na exibicao dos resultados e submissao dos dados de pesquisa

--- src/main/webapp/js/calcularEmissao.js
+++ src/main/webapp/js/calcularEmissao.js
@@ -1,37 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	$("#btnCalcular").click(function(){
-    
-    	var cep = $("#inputCep").val();
-	    
+    	calcularEmissao();
+	});
+	
+	$("#frmCalcular").submit(function() {
+		calcularEmissao();
+	});
+	
+	calcularEmissao = function() {
+		var cep = $("#inputCep").val();
 	    if(naoInformouCep(cep)) {
 	    	alert("Informe o seu CEP");
 	    }
 	    else {
 	    	cep = String(cep).replace("-", "");
-		    calcularEmissao(cep);
-		    $("#resultado").modal('toggle');
+			var uri = "services/emissao/" + cep;
+
+	        $.getJSON(uri, function(data) {
+	        	var transporte = data.meio_transporte + ",";
+	        	var distancia = data.distancia + " km,";
+	        	var emissao = data.emissao + " kg de CO2";
+	        	
+	        	$("#resultadoTransporte").html(transporte);
+	        	$("#resultadoDistancia").html(distancia);
+	        	$("#resultadoEmissao").html(emissao);
+	       	});
+			
+			var mensagemErro = data.mensagem;
+
+			if(mensagemErro == undefined || mensagemErro == "") {
+			    $("#resultado").modal('toggle');
+			}
+			else {
+				alert(data.mensagem);
+			}
 	    }
-	});
+	}
 	
 	naoInformouCep = function(cep) {
     	return cep == undefined || cep == "";
     };
     
-    calcularEmissao = function(cep) {
-    	
-    	var uri = "services/emissao/" + cep;
-
-        $.getJSON(uri, function(data) {
-
-        	var transporte = data.meio_transporte + ",";
-        	var distancia = data.distancia + " km,";
-        	var emissao = data.emissao + " kg de CO2";
-        	
-        	$("#resultadoTransporte").html(transporte);
-        	$("#resultadoDistancia").html(distancia);
-        	$("#resultadoEmissao").html(emissao);
-       	});
-    }
-
 });
