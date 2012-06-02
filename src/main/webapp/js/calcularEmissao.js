d41bdab8f8c72f2bb369ccc8d81db3cb51dca657
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2012-06-02 14:10:03 -0300

    exibindo o resultado da consulta à api de calculo de emissao no modal de resultados. eliminando js nao utilizados

--- src/main/webapp/js/calcularEmissao.js
+++ src/main/webapp/js/calcularEmissao.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	
 	$("#btnCalcular").click(function(){
+    
     	var cep = $("#inputCep").val();
 	    
 	    if(naoInformouCep(cep)) {
 	    	alert("Informe o seu CEP");
 	    }
 	    else {
+	    	cep = String(cep).replace("-", "");
 		    calcularEmissao(cep);
 		    $("#resultado").modal('toggle');
 	    }
@@ -17,11 +19,18 @@ $(document).ready(function(){
     };
     
     calcularEmissao = function(cep) {
+    	
     	var uri = "services/emissao/" + cep;
 
         $.getJSON(uri, function(data) {
-        	alert(JSON.stringify(data));
-        	var emissao = data.emissao;
+
+        	var transporte = data.meio_transporte + ",";
+        	var distancia = data.distancia + " km,";
+        	var emissao = data.emissao + " kg de CO2";
+        	
+        	$("#resultadoTransporte").html(transporte);
+        	$("#resultadoDistancia").html(distancia);
+        	$("#resultadoEmissao").html(emissao);
        	});
     }
 
